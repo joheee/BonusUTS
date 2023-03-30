40deef9af678a5100b7c6e4d2314e88f473e38b3
joheee <94522173+joheee@users.noreply.github.com>
2023-03-30 15:22:00 +0700

    added sign for each led

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,9 @@ DHTesp dht;
 BH1750 lightMeter;
 
 #define LED_FLASH   4
-#define LED_RED    15
-#define LED_YELLOW 14
-#define LED_GREEN  16
+#define LED_RED    16
+#define LED_YELLOW 17
+#define LED_GREEN  5
 #define PIN_SW     0
 #define PIN_DHT    13 
 
@@ -22,35 +22,57 @@ BH1750 lightMeter;
 #define PIN_SCL 33
 
 #define LED_COUNT 2
-const uint8_t arLed[LED_COUNT] = {LED_RED, LED_GREEN};
 
 
 int nCount=0;
+
+void displayTempHumidity(float humidity, float temperature){
+    Serial.printf("Temperature: %.2f C\n", temperature);
+    Serial.printf("Humidity: %.2f %%\n", humidity);
+    if(temperature > 28 && humidity >= 80) {
+      digitalWrite(LED_RED, HIGH);
+      digitalWrite(LED_YELLOW, LOW);
+      digitalWrite(LED_GREEN, LOW);
+    }
+    else if(temperature > 28 && (humidity > 60 && humidity < 80)) {
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_YELLOW, HIGH);
+      digitalWrite(LED_GREEN, LOW);
+    }
+    else if(temperature < 28 && humidity <= 60) {
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_YELLOW, LOW);
+      digitalWrite(LED_GREEN, HIGH);
+    }
+}
+
+void displayLight(float lux){
+  Serial.printf("Light: %.2f lx\n", lux);
+  if(lux > 400) Serial.println("pintu kebuka bos");
+  else Serial.println("pintu ketutup bang");
+}
 
 void onReadDht(){
   float humidity = dht.getHumidity();
   float temperature = dht.getTemperature();
 
   if (dht.getStatus()==DHTesp::ERROR_NONE){
-    Serial.printf("Temperature: %.2f C\n", temperature);
-    Serial.printf("Humidity: %.2f %%\n", humidity);
+    displayTempHumidity(humidity,temperature);
   }
 }
 
 void onReadLight(){
   float lux = lightMeter.readLightLevel();
-  Serial.printf("Light: %.2f lx\n", lux);
+  displayLight(lux);
 }
 
 void onReadSensors() {
-  digitalWrite(arLed[nCount%LED_COUNT], LED_ON);
   digitalWrite(LED_BUILTIN, LED_BUILTIN_ON);
   onReadDht();
   onReadLight();
   
   vTaskDelay(100/portTICK_PERIOD_MS);
   digitalWrite(LED_BUILTIN, LED_BUILTIN_OFF);
-  digitalWrite(arLed[nCount%LED_COUNT], LED_OFF);
   nCount++;
 }
 
@@ -64,18 +86,16 @@ void OnTimerReadDht(){
   float temperature = dht.getTemperature();
   
   if (dht.getStatus()==DHTesp::ERROR_NONE){
-    Serial.printf("Temperature: %.2f C\n", temperature);
-    Serial.printf("Humidity: %.2f %%\n", humidity);
+    displayTempHumidity(humidity,temperature);
+
   }
   vTaskDelay(100/portTICK_PERIOD_MS);
   digitalWrite(LED_BUILTIN, LED_BUILTIN_OFF);
 }
 
 void Bh1750Trial() {
-  for (uint8_t i=0; i<LED_COUNT; i++) pinMode(arLed[i], OUTPUT);
-  
   pinMode(PIN_SW, INPUT);
-  attachInterrupt(digitalPinToInterrupt(PIN_SW), onResetCounter,FALLING); // extra checking
+  attachInterrupt(digitalPinToInterrupt(PIN_SW), onResetCounter,FALLING);
   dht.setup(PIN_DHT, DHTesp::DHT11);
   Wire.begin(PIN_SDA, PIN_SCL);
   lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE, 0x23,  &Wire);
@@ -98,6 +118,9 @@ void executeBH(void *arg){
 void setup() {
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
 
   xTaskCreatePinnedToCore(executeBH,"BH1750",2048,NULL,1,NULL,0);
   xTaskCreatePinnedToCore(executeDHT,"DHT",2048,NULL,2,NULL,0);
